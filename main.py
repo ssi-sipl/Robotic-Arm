eded6fbee16d0b2c3a82d1337fa16b6b272753a3
vinithjay27 <vinjayanandan@gmail.com>
2025-05-14 19:19:30 +0530

    new

--- main.py
+++ main.py
@@ -80,24 +80,24 @@ kit.servo[13].angle = 0
 
 time.sleep(2)
 
-kit.servo[12].angle = 0
-print("Servo 3 at 0")
-time.sleep(2)
-kit.servo[12].angle = 45
-print("Servo 3 at 45")
-time.sleep(2)
-kit.servo[12].angle = 90
-print("Servo 3 at 90")
+# kit.servo[12].angle = 0
+# print("Servo 3 at 0")
+# time.sleep(2)
+# kit.servo[12].angle = 45
+# print("Servo 3 at 45")
+# time.sleep(2)
+# kit.servo[12].angle = 90
+# print("Servo 3 at 90")
 
-# for i in range(10,170,10):
-#     print("Moving Up at ",i) 
-#     kit.servo[12].angle = i
-#     time.sleep(1)
+for i in range(0,170,10):
+    print("Moving Up at ",i) 
+    kit.servo[12].angle = i
+    time.sleep(1)
 
-# for i in range(160,80,-10):
-#     print("Moving Down at ",i)
-#     kit.servo[12].angle = i
-#     time.sleep(1)
+for i in range(160,0,-10):
+    print("Moving Down at ",i)
+    kit.servo[12].angle = i
+    time.sleep(1)
 
 
 
