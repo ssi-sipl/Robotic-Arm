f96c440a98b222e1cbddea1388b32464b0916736
vinithjay27 <vinjayanandan@gmail.com>
2025-05-14 18:28:51 +0530

    new

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ safe_angles = {
 #     time.sleep(0.3)  # Small delay between each servo
             
 
-kit.servo[15].set_pulse_width_range(min_pulse=600, max_pulse=2400)
+kit.servo[14].set_pulse_width_range(min_pulse=600, max_pulse=2400)
 kit.servo[14].angle = 30
 
 # Example usage
