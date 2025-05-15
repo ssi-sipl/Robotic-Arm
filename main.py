ebf9c58cb5e8015fb65521ca14121824ab70afcb
vinithjay27 <vinjayanandan@gmail.com>
2025-05-15 12:51:28 +0530

    update

--- main.py
+++ main.py
@@ -46,19 +46,22 @@ def set_servo_angle(index, angle):
 # kit.servo[15].angle = 90
 # time.sleep(0.5)
 
-kit.continuous_servo[11].throttle = 1
-time.sleep(2)
-kit.continuous_servo[11].throttle = 0
-time.sleep(2)
-kit.continuous_servo[10].throttle = 1
-time.sleep(2)
+# kit.continuous_servo[11].throttle = 1
+# time.sleep(2)
+# kit.continuous_servo[11].throttle = 0
+# time.sleep(2)
+# kit.continuous_servo[11].throttle = 1
+# time.sleep(2)
 
 
 kit.servo[10].angle = 90
+print("Servo 10 set to 90 degrees")
 time.sleep(2)
 kit.servo[10].angle = 180
+print("Servo 10 set to 180 degrees")
 time.sleep(2)
 kit.servo[10].angle = 90
+print("Servo 10 set to 90 degrees")
 time.sleep(2)
 
 # kit.continuous_servo[11].set_pulse_width_range(min_pulse=990, max_pulse=2010)
