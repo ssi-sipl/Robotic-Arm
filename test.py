2384d33bd5ddffdb30fccd984223ec4a50ce3db4
vinithjay27 <vinjayanandan@gmail.com>
2025-05-20 15:49:58 +0530

    message

--- test.py
+++ test.py
@@ -5,31 +5,31 @@ import json
 # Initialize PCA9685 with 16 channels
 kit = ServoKit(channels=16)
 
-with open('./config.json', 'r') as config_file:
-    config = json.load(config_file)
+# with open('./config.json', 'r') as config_file:
+#     config = json.load(config_file)
 
-def initial_positions():
-    for key, value in config.items():
-        kit.servo[int(key)].angle = value.get('initial')
+# def initial_positions():
+#     for key, value in config.items():
+#         kit.servo[int(key)].angle = value.get('initial')
  
-def starting_animation():
-    kit.servo[15].angle = 90
-    time.sleep(0.5)
-    kit.servo[15].angle = 180
-    time.sleep(0.5)
-    kit.servo[15].angle = 90
-    time.sleep(0.5)
+# def starting_animation():
+#     kit.servo[15].angle = 90
+#     time.sleep(0.5)
+#     kit.servo[15].angle = 180
+#     time.sleep(0.5)
+#     kit.servo[15].angle = 90
+#     time.sleep(0.5)
 
 # initial_positions()
 # starting_animation()
 
 for i in range(0, 190, 10):
-    kit.servo[11].angle = i
-    print(f"Servo 11 angle: {i}")
+    kit.servo[15].angle = i
+    print(f"Servo 15 angle: {i}")
     time.sleep(1)
 for i in range(180, 0, -10):
-    kit.servo[11].angle = i
-    print(f"Servo 11 angle: {i}")
+    kit.servo[15].angle = i
+    print(f"Servo 15 angle: {i}")
     time.sleep(1)
 
 
